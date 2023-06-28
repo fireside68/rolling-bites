0a232ec80d4192bc1a8a82e5b734aa83669c1144
Cedric Johnson <cedric.johnson@revzilla.com>
2023-06-27 21:49:09 -0600

    Update tests/remove unused tests

--- test/http_client_mock.ex
+++ test/http_client_mock.ex
@@ -1,5 +1,5 @@
 defmodule RollingBitesWeb.HTTPClientMock do
   use Mox
-  # This line is specifying that this mock will implement the RollingBites.HTTPClient behaviour.
-  impl RollingBitesWeb.HTTPClient
+  # This line is specifying that this mock will implement the RollingBitesWeb.HTTPClient behaviour.
+  impl(RollingBitesWeb.HTTPClient)
 end
