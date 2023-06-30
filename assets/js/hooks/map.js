eee8f6ef1995cd448df8f4766b055c6b65efffd1
Cedric Johnson <cjohns0913@gmail.com>
2023-06-30 16:08:50 -0600

    Map appears finally

--- assets/js/hooks/map.js
+++ assets/js/hooks/map.js
@@ -2,15 +2,11 @@ import L from "leaflet"
 
 const Map = {
   mounted() {
-    console.log("Map hook mounted")
     var latitude = this.el.dataset.lat;
     var longitude = this.el.dataset.lng;
     var name = this.el.dataset.name;
-    console.log(latitude);
-    console.log(longitude);
-    console.log(name);
 
-    var map = L.map('map').setView([latitude, longitude], 13);
+    var map = L.map('map').setView([latitude, longitude], 17);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
