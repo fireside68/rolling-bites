40d2212809f69d50b4f2c703b0a08e237e594dd4
Cedric Johnson <cedric.johnson@revzilla.com>
2023-07-15 20:03:57 -0600

    Get user location

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,7 +1,17 @@
 import Map from "./hooks/map";
+import { getUserLocation } from "./hooks/geolocation"
+
 
 let Hooks = {
   Map: Map
 };
+
+Hooks.GetLocation = {
+  mounted() {
+    getUserLocation().then(location => {
+      this.pushEvent("got_location", location);
+    });
+  }
+}
 
 export default Hooks;
