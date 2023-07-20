904d79607c38215f53cd377c347664f471af37e1
Cedric Johnson <cedric.johnson@revzilla.com>
2023-07-19 18:30:37 -0600

    14: Update JS entrypoints

--- assets/esbuild.js
+++ assets/esbuild.js
@@ -1,7 +1,13 @@
 const esbuild = require("esbuild");
 
 const options = {
-  entryPoints: ["js/app.js"],
+  entryPoints: [
+    "js/app.js",
+    "js/hooks.js",
+    "js/hooks/geolocation.js",
+    "js/hooks/index-map.js",
+    "js/hooks/map.js"
+  ],
   bundle: true,
   minify: true,
   target: ["es2017"],
